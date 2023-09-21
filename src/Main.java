bcc2c9a9460bfed6319e8b55a36634d781f83f5d
ajdooley18 <ajdooley16@gmail.com>
2023-09-21 15:28:47 -0400

    Lab 05, Task 1, Ship Cost Calculator

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,27 @@
-public class Main {
+import java.util.Scanner;
+
+public class Main
+{
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the price of your item: ");
+        double itemPrice = scanner.nextDouble();
+        double shipping = 0;
+
+        if(itemPrice >= 100)
+        {
+            shipping = 0;
+        }
+        else
+        {
+            shipping = itemPrice * 0.02;
+        }
+
+        double finalCost = itemPrice + shipping;
+        System.out.println("The shipping cost is " + shipping + " and the total price is " + finalCost);
+
+        scanner.close();
+
     }
 }
